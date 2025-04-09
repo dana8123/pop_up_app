213f342f3fd32283802cc9cc641171ae8b3159e2
danaisboss <yjk9313@gmail.com>
2025-04-09 17:36:44 +0900

    Feature, 공유하기 버튼 추가

--- lib/screens/popup_list_page.dart
+++ lib/screens/popup_list_page.dart
@@ -3,6 +3,7 @@ import 'package:popup_app/utils/date_helper.dart';
 import 'package:popup_app/utils/like_helper.dart';
 import 'package:popup_app/utils/tag_color_helper.dart';
 import 'package:provider/provider.dart';
+import 'package:share_plus/share_plus.dart';
 import '../providers/popup_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -163,8 +164,21 @@ class _PopupListPageState extends State<PopupListPage> {
                                       onPressed: () =>
                                           _openLink(context, popup.googleMap),
                                     ),
+                                    // 👉 왼쪽 아이콘들 끝나고 공간 밀어냄
+                                    Spacer(),
+                                    IconButton(
+                                      icon: Icon(Icons.share),
+                                      onPressed: () {
+                                        final shareText = 
+                                    '''Popup Finder\n📍 ${popup.name}\n📌 ${popup.address ?? '주소 정보 없음'}\n🗓️ ${formatPopupDateFromString(popup.startDate)} ~ ${formatPopupDateFromString(popup.endDate)}\n지금 이 팝업, 딱 내 취향...!  
+👉 Popup Finder에서 더 알아보기!''';
+
+                                        Share.share(shareText);
+                                      },
+                                    ),
                                   ],
                                 ),
+
                               ],
                             ),
                           ),
